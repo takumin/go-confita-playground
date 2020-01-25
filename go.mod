fd6ab3d73ef16f03607ec862c82fa333dbcd07d2
takumin <takumiiinn@gmail.com>
2020-01-26 00:01:28 +0900

    initial import

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/takumin/go-confita-playground
+
+go 1.13
+
+require github.com/heetch/confita v0.9.0
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/heetch/confita"
+	"github.com/heetch/confita/backend/env"
+	"github.com/heetch/confita/backend/flags"
+)
+
+type Config struct {
+	Host string `config:"host"`
+	Port uint32 `config:"port"`
+}
+
+func main() {
+	loader := confita.NewLoader(
+		env.NewBackend(),
+		flags.NewBackend(),
+	)
+
+	cfg := Config{
+		Host: "127.0.0.1",
+		Port: 5656,
+	}
+
+	if err := loader.Load(context.Background(), &cfg); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(cfg.Host)
+	log.Println(cfg.Port)
+}
